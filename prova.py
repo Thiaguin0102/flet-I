333c4d9d5695dabd7ad891be2cdcd0735ae04fd4
Thiaguin0102 <tfeitosa214@gmail.com>
2024-10-25 15:01:53 -0300

    Update prova.py

--- prova.py
+++ prova.py
@@ -1,16 +1,13 @@
 import flet as ft
 
 def main(page: ft.Page):
-    # Configuração da página
     page.title = "Lista de Tarefas"
     page.vertical_alignment = ft.MainAxisAlignment.START
 
-    # Criando componentes da interface
     tarefa_input = ft.TextField(label="Nova Tarefa", width=300)
     add_button = ft.ElevatedButton("Adicionar", on_click=lambda e: add_tarefa())
     tarefa_lista = ft.Column()
-
-    # Função para adicionar tarefa à lista
+    
     def add_tarefa():
         tarefa_text = tarefa_input.value
         if tarefa_text:
@@ -19,8 +16,7 @@ def main(page: ft.Page):
             tarefa_input.focus()  # Retorna o foco ao campo de entrada
             page.update()  # Atualiza a página
 
-    # Organizando os componentes na página
     page.add(tarefa_input, add_button, tarefa_lista)
 
-# Executando a aplicação
+
 ft.app(target=main)
